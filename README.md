48b7684bfff4b9c016da3ae09d74660edbc3c296
AnthonyAmaker <tamaker@gmail.com>
2019-03-18 19:31:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# JWT using jsonwebtoken
+
+* generating and validating JSON web token in node.js
+
+
+## Generate token
+`var  token  =  jwt.sign(tokenPayload, key); // CREATE THE TOKEN`
+`jwt.verify(token, key);  // DECODE THE TOKEN, REQUIRES TOKEN AND KEY`
+
+## NOT meant to store / transport secure data...
+*
---
+++ server.js
@@ -0,0 +1,40 @@
+var jwt = require('jsonwebtoken');
+var moment = require('moment');
+
+// KEY THAT
+var key = '12345ASDFas@%@$@#$adsfa9w9f9f9e9wetwt24234';
+
+var iat = moment(new Date()).unix();
+var exp = moment(new Date()).add(1, 'hours').unix();
+
+var userName = 'mickey mouse';
+
+var tokenPayload = {
+    "iss": "HEPbuilder",    // ISSUER
+    "aud": 'test users 1',  // AUDIENCE
+    "iat": iat,             // ISSUED AT
+    "exp": exp,             // EXPIRES AT
+    "userName": userName,    // OUR USERS NAME
+    "userOtherValue": 'test information for user'
+};
+
+
+var iat = new Date(); // ISSUED AT DATE
+var exp = new Date(); // EXPIRES AT DATE
+
+var token = jwt.sign(tokenPayload, key);  // CREATE THE TOKEN
+
+console.log('JWT Generated: ' + token); // ECHO OUT THE TOKEN 
+
+
+console.log('-----------------')
+
+
+// ATTEMPT TO DECODE THE JSON WEB TOKEN, REQUIRES WE HAVE THE TOKEN AND KEY
+try {
+    var decoded = jwt.verify(token, key);
+    console.log(decoded)
+} catch(err) {
+    console.log('ERROR validating token');
+    console.log(err.name + ': ' + err.message);
+} 
