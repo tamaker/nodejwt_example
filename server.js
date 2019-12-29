12f0b6a60d41a1c4bb497447d296711894d9b4e1
aamaker <tamaker@users.noreply.github.com>
2019-12-29 09:41:29 -0500

    cleanup and tweaks prior to making this public

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-var dotenv = require('dotenv').config()
-var jwt = require('jsonwebtoken');
-var moment = require('moment');
+var dotenv = require('dotenv').config() // USED TO ISOLATE / PROTECT SIGNING KEY FOR TOKEN
+var jwt = require('jsonwebtoken');      // USED FOR JSON WEB TOKEN CREATE / VALIDATE
+var moment = require('moment');         // USED FOR DATE/TIMES
 
-// KEY THAT
+// KEY THAT IS USED TO SIGN THE TOKEN
 var key = process.env.JWT_KEY;
 
+// ISSUED AT DATE
 var iat = moment(new Date()).unix();
-//var exp = moment(new Date()).add(1, 'hours').unix();
+// EXPIRES AT DATE
 var exp = moment(new Date()).add(3, 'seconds').unix();
 
 var userName = 'mickey mouse';
 
 var tokenPayload = {
-    "iss": "HEPbuilder",    // ISSUER
+    "iss": "SOME COMMON STRING",    // ISSUER
     "aud": 'test users 1',  // AUDIENCE
     "iat": iat,             // ISSUED AT
     "exp": exp,             // EXPIRES AT
@@ -22,10 +23,7 @@ var tokenPayload = {
 };
 
 
-var iat = new Date(); // ISSUED AT DATE
-var exp = new Date(); // EXPIRES AT DATE
-
-var token = jwt.sign(tokenPayload, key);  // CREATE THE TOKEN
+var token = jwt.sign(tokenPayload, key);  // CREATE THE TOKEN, AND SIGN WITH KEY
 
 console.log('JWT Generated: ' + token); // ECHO OUT THE TOKEN 
 
@@ -44,17 +42,16 @@ try {
     console.log('ERROR validating token! ' + err.name + ': ' + err.message);
 } 
 
-
+// WAIT UNTIL THE TOKEN HAS EXPIRED, RETRY AUTH AND IT SHOULD FAIL
+// (NOTE THE TOKEN EXP IS SET TO 3 SECS, WE WAIT 5 SECS HERE)
 setTimeout(function(){
-
     try {
         var decoded = jwt.verify(token, key);
-        // var decoded = jwt.verify('token', key);
-        //var decoded = jwt.verify(token, '23456');   // THROWS AND ERROR BACK BECAUSE WRONG KEY
+        //var decoded = jwt.verify(token, '23456');   // THROWS AND ERROR BACK BECAUSE WRONG KEY -- ONLY WE KNOW OUR KEY!
         console.log('Successfully validated token!');
         console.log(decoded);
     } catch(err) {
-        //console.log(decoded)
+        // SHOW THE AUTH REJECTION DETAILS (MESSAGE AND NAME)
         console.log('ERROR validating token! ' + err.name + ': ' + err.message);
     } 
 
